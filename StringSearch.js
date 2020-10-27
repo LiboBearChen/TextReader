d81b4b7843a493e34f0c2fed339302e48a321bb7
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-10-27 16:39:39 -0400

    update

--- StringSearch.js
+++ StringSearch.js
@@ -1,4 +1,23 @@
 function loadBook(fileName,displayName){
     let currentBook="";
-    
+    let url="books/"+fileName;
+
+    document.getElementById("fileName").innerHTML=displayName;
+    document.getElementById("searchstat").innerHTML="";
+    document.getElementById("keyword").value="";
+
+    var xhr=new XMLHttpRequest();
+    xhr.open("GET",url,true);
+    xhr.send();
+
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState==4 && xhr.status==200){
+            currentBook=xhr.responseText;
+            
+            document.getElementById("fileContent").innerHTML=currentBook;
+
+            var elmnt=document.getElementById("fileContent");
+            elmnt.scrollTop=0;
+        }
+    };
 }
